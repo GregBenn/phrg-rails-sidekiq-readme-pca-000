75fc963b5f0b909b39d02c80b2c4974c8e4b80f4
Greg Benn <gbenn07@gmail.com>
2018-10-02 10:07:46 -0400

    Done.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,6 +5,15 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  # def upload
+  # removed and placed into the worker perform method, except (params[:leads].path)
+  #   CSV.foreach(params[:leads].path, headers: true) do |lead|
+  #     Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+  #   end
+
+  #   redirect_to customers_path
+  # end
+
   def upload
     LeadsWorker.perform_async(params[:leads].path)
     redirect_to customers_path
--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -3,6 +3,7 @@ class LeadsWorker
   include Sidekiq::Worker
 
   def perform(leads_file)
+    # replaced params[:leads].path with leads_file
     CSV.foreach(leads_file, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
